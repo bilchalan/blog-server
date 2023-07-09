a6394f3d6d86e3778e5967a1a835b91d56155931
bilchalan <bilchalan@gmail.com>
2023-07-10 01:19:25 +0600

    update main

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://bil-blog.onrender.com'],
+    origin: ['http://localhost:3000', '*'],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
     credentials: true,
   });
